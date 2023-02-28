377ef6e2d8f80e47a48847a19e3dd383ab5bb605
NuOrawan <117319337+NuOrawan@users.noreply.github.com>
2023-02-28 15:14:33 -0800

    Update server.js to include http, fs , url module and add log to log file

--- src/server.js
+++ src/server.js
@@ -1,8 +1,36 @@
-// Import http module and listens for request on port 8080
-const http = require('http');
+
+const url = require('url'),
+    http = require('http'),
+    fs = require('fs');
+/* Listens for request on port 8080.
+Determine if the URL contains the word “documentation”. 
+If it does, return the “documentation.html” file to the user; otherwise return the “index.html” file.*/
 http.createServer((request, response)=> {
-    // Create header with response
-    response.writeHead(200, {'Content-type' :'text/plain'});
-    response.end('Hello \n');  
+    let addr = request.url,
+    q = url.parse(addr,true);
+    filePath = '';
+    // Add to log file
+    fs.appendFile('log.txt', 'URL' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) =>{
+        if (err){
+            console.log(err);
+        } else {
+            console.log('Add to log.')
+        }
+    });
+    if (q.pathname.includes('documentation')){
+        filePath = (__dirname + '/documentation.html');
+    } else {
+        filePath = 'index.html';
+    }
+    fs.readFile(filePath, (err, data) =>{
+        if(err){
+            throw err;
+        }
+        // Create header with response
+        response.writeHead(200, {'Content-type' :'text/plain'});
+        response.write(data);
+        response.end();  
+    });
+    
 }).listen(8080);
-console.log('Server is running successfully.');
+console.log('Test Server is running successfully on port 8080.');
