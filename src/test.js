71401cdf65ef0689843258b1f78c6909eb461c45
NuOrawan <117319337+NuOrawan@users.noreply.github.com>
2023-03-01 12:25:18 -0800

    Create index.js and install Express and body-parser

--- src/test.js
+++
@@ -1,9 +0,0 @@
-let msg = 'Hello Node';
-console.log(msg);
-
-console.log('Goodbye');
-import { Buffer } from "buffer";
-const bufs = Buffer.from([1,2,3,4]);
-for (const buf of bufs){
-    console.log(buf);
-};
