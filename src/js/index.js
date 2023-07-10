1b2da519da509159369b7a3839167f6d2556cf70
NuOrawan <117319337+NuOrawan@users.noreply.github.com>
2023-07-09 17:47:20 -0700

    Update CONNECTION URI

--- src/js/index.js
+++ src/js/index.js
@@ -18,7 +18,9 @@ const { check , validationResult} = require('express-validator');
 
 
 // Connect to database
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connect( process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(bodyParser.json());
 // CORS
